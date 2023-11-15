66e30ca4d417a37467a5490bd5e78ef37519c0e6
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-15 11:08:39 +0100

    ADD > organized the setup with all dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { 
+  faFacebookF, 
+  faTwitter, 
+  faLinkedinIn, 
+  faInstagram,
+  faGooglePlusG
+ } from '@fortawesome/free-brands-svg-icons'
+
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(
+  faFacebookF, 
+  faTwitter, 
+  faLinkedinIn, 
+  faInstagram,
+  faGooglePlusG,
+  faLock
+)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
