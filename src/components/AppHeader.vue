21bd72648c4170daa5d12109c4b7b803991ce822
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-15 14:49:02 +0100

    ADD > navbar (draft)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,49 @@
 <script>
 import SocialAndLogin from './HeaderComponents/SocialAndLogin.vue';
 import SearchInputHeader from './HeaderComponents/SearchInputHeader.vue';
+import NavBar from './HeaderComponents/NavBar.vue';
 
 export default {
 
   components: {
     SocialAndLogin,
-    SearchInputHeader
+    SearchInputHeader,
+    NavBar
   },
 
   data() {
     return {
-      message: 'AppHeader.vue'
+      message: 'AppHeader.vue',
+      navData: [
+        {
+          text: 'Home',
+          link: '#'
+        },
+        {
+          text: 'Celebrity',
+          link: '#'
+        },
+        {
+          text: 'Movie',
+          link: '#'
+        },
+        {
+          text: 'Page',
+          link: '#'
+        },
+        {
+          text: 'Shop',
+          link: '#'
+        },
+        {
+          text: 'Blog',
+          link: '#'
+        },
+        {
+          text: 'Contacts Us',
+          link: '#'
+        },
+      ]
     }
   }
 }
@@ -23,6 +55,7 @@ export default {
   <header class="page-header">
     <SocialAndLogin/>
     <SearchInputHeader/>
+    <NavBar :navData="navData"/>
   </header>
 
 </template>
---
+++ src/components/HeaderComponents/NavBar.vue
@@ -0,0 +1,64 @@
+<script>
+
+export default {
+  props: {
+    navData: {
+      type: Array,
+      required: true
+    }
+  },
+}
+
+</script>
+
+<template>
+
+  <section class="section section-padding">
+    <div class="container">
+      <nav class="navbar flex justify-between">
+
+        <ul class="nav-items flex">
+          <li v-for="(item, i) in navData" :key="i" class="nav-item">
+            <a :href="item.link">{{ item.text }}</a>
+          </li>
+        </ul>
+
+        <div class="nav-shop">
+          <font-awesome-icon :icon="['fas', 'basket-shopping']" />
+          <span class="num">0</span>
+        </div>
+
+      </nav>
+    </div>
+  </section>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+  .section {
+    border-bottom: 2px solid $borderBlue;
+    .navbar {
+      padding: 25px 0;
+      .nav-items {
+        gap: 30px;
+      }
+
+      .nav-shop {
+        position: relative;
+        .num {
+          position: absolute;
+          right: -5px;
+          top: -10px;
+          z-index: 10;
+          font-size: 12px;
+          background-color: $green;
+          border-radius: 50%;
+          padding: 0 3px;
+        }
+      }
+    }
+  }
+
+</style>
