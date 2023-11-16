552410c3c5ee636fe199f61a8eb185772da7fc0b
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-16 12:16:16 +0100

    ADD > component card for trailers

--- src/components/MainComponents/MoviePlaylist.vue
+++ src/components/MainComponents/MoviePlaylist.vue
@@ -1,9 +1,11 @@
 <script>
 import AppBanner from './AppBanner.vue';
+import TrailerCard from './TrailerCard.vue';
 
 export default {
   components: {
-    AppBanner
+    AppBanner,
+    TrailerCard
   }
 }
 </script>
@@ -20,19 +22,8 @@ export default {
             <h4>New movie</h4>
             <span>playing 24</span>
           </div>
-          <!-- TODO: TRAILER CARD COMPONENT -->
           <ul class="trailers-list">
-            <li class="trailer flex">
-              <img src="../../../img/2-100x100.jpg" alt="">
-              <div class="trailer__description flex flex-column">
-                <h5>Lorem ipsum dolor sit. Lorem, ipsum.</h5>
-                <strong class="views">324 views</strong>
-                <div class="date flex justify-between">
-                  <span>February 12, 2016</span>
-                  <span>2.30.000</span>
-                </div>
-              </div>
-            </li>
+            <TrailerCard :time="'2.30.000'"/>
           </ul>
         </div>  
       </div>
@@ -68,17 +59,6 @@ export default {
 
       .trailers-list {
         overflow: auto;
-        .trailer {
-          gap: 10px;
-          margin-bottom: 20px;
-
-          &__description {
-            gap: 10px;
-            .views {
-              font-size: 13px;
-            }
-          }
-        }
       }
 
       & > * {
---
+++ src/components/MainComponents/TrailerCard.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+  props: {
+    time: {
+      type: String,
+      default() {
+        return '';
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <li class="trailer flex">
+    <img src="../../../img/2-100x100.jpg" alt="">
+    <div class="trailer__description flex flex-column">
+      <h5>Lorem ipsum dolor sit. Lorem, ipsum.</h5>
+      <strong class="views">324 views</strong>
+      <div class="date flex justify-between">
+        <span>February 12, 2016</span>
+        <span>{{ time }}</span>
+      </div>
+    </div>
+  </li>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+  .trailer {
+    gap: 10px;
+    margin-bottom: 20px;
+
+    &__description {
+      gap: 10px;
+      .views {
+        font-size: 13px;
+      }
+
+      .date > * {
+        font-size: 15px;
+      }
+    }
+  }
+
+</style>
